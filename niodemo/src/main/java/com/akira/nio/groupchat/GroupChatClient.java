956b49734d4b4dd9e82098f18dab3df9b148837e
akira <zk_tao@163.com>
2021-08-08 00:32:46 +0800

    :construction: group chat demo

---
+++ niodemo/src/main/java/com/akira/nio/groupchat/GroupChatClient.java
@@ -0,0 +1,92 @@
+package com.akira.nio.groupchat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import static java.nio.channels.SelectionKey.OP_READ;
+
+/**
+ *
+ * @author 陶真凯
+ * @date 2021-08-07 23:41:44
+ * @description
+ */
+public class GroupChatClient {
+
+    public static final String HOST = "127.0.0.1";
+    public static final Integer PORT = 8888;
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private String username;
+
+    public GroupChatClient() throws IOException {
+        selector = this.selector.open();
+        socketChannel = SocketChannel.open(new InetSocketAddress(HOST, PORT));
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, OP_READ);
+        username = socketChannel.getLocalAddress().toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        //启动我们客户端
+        GroupChatClient chatClient = new GroupChatClient();
+        //启动一个线程,每个 3 秒，读取从服务器发送数据
+        new Thread(() -> {
+            while (true) {
+                chatClient.getRemoteMsg();
+                try {
+                    Thread.currentThread().sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        //发送数据给服务器端
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            chatClient.sendMsg(s);
+        }
+    }
+
+    private void sendMsg(String msg) {
+        msg = username + " 说：" + msg;
+        try {
+            socketChannel.write(ByteBuffer.wrap(msg.getBytes()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getRemoteMsg() {
+
+        try {
+            int count = selector.select();
+            if (count > 0) {
+                Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+                    if (key.isReadable()) {
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        channel.read(buffer);
+                        String msg = new String(buffer.array());
+                        System.out.println("receive an msg : " + msg);
+                    }
+                    iter.remove();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ niodemo/src/main/java/com/akira/nio/groupchat/GroupChatServer.java
@@ -0,0 +1,113 @@
+package com.akira.nio.groupchat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+/**
+ *
+ * @author 陶真凯
+ * @date 2021-08-07 19:44:53
+ * @description
+ */
+public class GroupChatServer {
+
+    public static final Integer PORT = 8888;
+
+    private Selector selector;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    public GroupChatServer() throws IOException {
+        this.selector = Selector.open();
+        this.serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress(PORT);
+        this.serverSocketChannel.configureBlocking(false);
+        this.serverSocketChannel.socket().bind(address);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 创建服务器对象
+        GroupChatServer groupChatServer = new GroupChatServer();
+        groupChatServer.listen();
+    }
+
+    public void listen() {
+        while (true) {
+            try {
+                int count = selector.select();
+                if (count > 0) {
+                    Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                    while (iter.hasNext()) {
+                        SelectionKey key = iter.next();
+                        if (key.isAcceptable()) {
+                            SocketChannel clientChannel = serverSocketChannel.accept();
+                            clientChannel.configureBlocking(false);
+                            clientChannel.register(selector, SelectionKey.OP_READ);
+                            // online tips
+                            System.out.println("client: " + clientChannel.getRemoteAddress() + " is online! ");
+                        }
+
+                        if (key.isReadable()) {
+                            // read channel data
+                            readClientMsg(key);
+                        }
+                        // 当前的 key 删除，防止重复处理
+                        iter.remove();
+                    }
+                } else {
+                    System.out.println("waiting the person with a destiny...");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readClientMsg(SelectionKey key) {
+        SocketChannel clientChannel = null;
+        try {
+            clientChannel = (SocketChannel) key.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            // read msg from channel to buffer
+            int length = clientChannel.read(buffer);
+            if (length > 0) {
+                String msg = new String(buffer.array());
+                System.out.format("server get msg from client(%s) : %s\n", clientChannel.getRemoteAddress(), msg);
+
+                // send msg to other clients
+                sendMsgToOtherClients(msg, clientChannel);
+            }
+        } catch (IOException e) {
+            try {
+                System.out.println(clientChannel.getRemoteAddress() + "is offline...");
+                //取消注册
+                key.cancel();
+                //关闭通道
+                clientChannel.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    private void sendMsgToOtherClients(String msg, SocketChannel self) throws IOException {
+        System.out.println("server send msg to others...");
+        // 遍历所有注册到 selector 上的 SocketChannel,并排除 self
+        for (SelectionKey key : selector.keys()) {
+            // get channel
+            Channel targetChannel = key.channel();
+            // exclude self
+            if (targetChannel instanceof SocketChannel && targetChannel != self) {
+                SocketChannel dest = (SocketChannel) targetChannel;
+                // send msg to buffer
+                ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+                // writer msg from buffer to channel
+                dest.write(buffer);
+            }
+        }
+    }
+}
