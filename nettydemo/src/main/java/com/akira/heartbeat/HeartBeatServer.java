dfa07ed1cb0d396d6ca4f099172fc5c41cb9cb7a
akira <zk_tao@163.com>
2021-08-23 19:23:49 +0800

    :construction: netty heartbeat check

---
+++ nettydemo/src/main/java/com/akira/heartbeat/HeartBeatServer.java
@@ -0,0 +1,53 @@
+package com.akira.heartbeat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author akira
+ * @date 2021-08-23 19:15:03
+ * @description
+ */
+public class HeartBeatServer {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+                    // netty 提供的心跳检测器
+                    pipeline.addLast(new IdleStateHandler(3,5,7, TimeUnit.SECONDS));
+
+                    pipeline.addLast(new HeartBeatHandler());
+                }
+            });
+
+            ChannelFuture future = serverBootstrap.bind(9999).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
